57adde0bb2f5940320284c7b63fe319e67ac82d6
Simon Flohr <sflohr2@wgu.edu>
2023-12-11 08:11:38 -0600

    Add basic content to the 'recipe-list' component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,5 +8,7 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes:Recipe[] = [];
+  recipes:Recipe[] = [
+    new Recipe('Babi Kecap', 'Indonesian dish that uses pork belly and sweet soy sauce.', 'https://i0.wp.com/www.thegluttonlife.com/wp-content/uploads/2020/10/babi-kecap-soy-braised-pork-06.jpg?w=1024&ssl=1')
+  ];
 }
