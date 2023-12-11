8bb2b37c0aa97eed7b1c7b4d6d6f56b0de6a137c
Simon Flohr <sflohr2@wgu.edu>
2023-12-11 06:42:29 -0600

    Correct path for recipe.model.ts
    Correct path to `./src/app/recipes/recipe.model.ts' and created empty Recipe array in recipe-list.component.ts.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes:Recipe[] = [];
 }
