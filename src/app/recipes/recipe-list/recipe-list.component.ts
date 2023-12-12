203e359ffd453df9b69e25748508561cd6a87b75
Simon Flohr <sflohr2@wgu.edu>
2023-12-11 18:13:45 -0600

    Use directive to display recipe items in the recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes:Recipe[] = [
-    new Recipe('Babi Kecap', 'Indonesian dish that uses pork belly and sweet soy sauce.', 'https://i0.wp.com/www.thegluttonlife.com/wp-content/uploads/2020/10/babi-kecap-soy-braised-pork-06.jpg?w=1024&ssl=1')
+    new Recipe('Babi Kecap', 'Indonesian dish that uses pork belly and sweet soy sauce.', 'https://i0.wp.com/www.thegluttonlife.com/wp-content/uploads/2020/10/babi-kecap-soy-braised-pork-06.jpg?w=1024&ssl=1'),
+    new Recipe('Ayam Pedis', 'Indonesian spicy chicken dish.', 'https://cdn.webshopapp.com/shops/110638/files/357937279/ayam-pedis-kip-met-witte-rijst-gado-gado-en-kokos.jpg')
   ];
 }
